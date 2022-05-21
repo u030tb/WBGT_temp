47591d1832ac38f1096015a2d993e2c487e89736
u030tb <104076641+u030tb@users.noreply.github.com>
2022-05-21 10:23:36 +0900

    Add files via upload

---
+++ 2_prefectural_BLUP.R
@@ -0,0 +1,489 @@
+source("R_code/1_prefectural_data.R")
+
+regional_division
+pref_list
+metapredictor_table
+pref_vec
+region_vec_level
+
+###############################################################################
+# 
+# first-stage analysis
+# 
+###############################################################################
+
+### set parameters for first-stage analysis ---------------------------------
+# heat-response relationship
+varfun <- "ns"
+
+# knots for exposure-response association
+varper <- c(50, 90)
+
+# lag-response association
+laglag <- 10
+lagnk <- 2
+
+# df of ns for within-summer seasonality
+# -------------------------------------------------------------------------
+df_ns_dos <- 2
+# -------------------------------------------------------------------------
+
+
+# statistical model -------------------------------------------------------
+# reduced coef & vcov -----------------------------------------------------
+
+first_result_list <- 
+  pref_list %>% 
+  
+  ### temp ###
+  mutate(first_prepre_temp = map(data, ~{
+    
+    # data ------------------------------------------------------------
+    f_data = .x
+    
+    # crossbasis ------------------------------------------------------
+    f_cb_temp <- 
+      crossbasis(f_data$tmax, 
+                 
+                 lag=laglag,
+                 argvar=list(fun=varfun,
+                             knots=quantile(f_data$tmax,varper/100,na.rm=T),
+                             Bound = range(f_data$tmax, na.rm = T)),
+                 arglag=list(fun=varfun,
+                             knots=logknots(laglag, lagnk)),
+                 group = f_data$year)
+    
+    # statmodel -------------------------------------------------------
+    f_model_temp <- 
+      glm(all ~ f_cb_temp + year + dow + 
+            ns(dos,knots=quantile(dos_vec,
+                                  probs=seq(df_ns_dos)/(df_ns_dos+1))):factor(year),
+          data = f_data, 
+          family = quasipoisson)
+    
+    # crossreduce for first_stage -------------------------------------
+    # reference := mean() ~ (?ߋ??̐l?דI?????R???̎??Ґ?????
+    f_crossreduce_temp <- 
+      crossreduce(f_cb_temp, f_model_temp, 
+                  cen = mean(f_data$tmax,na.rm=T),
+                  by = 0.1)
+    
+    # first-stage curve -----------------------------------------------
+    f_crosspred_temp <- 
+      crosspred(f_cb_temp,f_model_temp,
+                by=0.1,cen = mean(f_data$tmax,na.rm=T))
+    f_MMT <- 
+      f_crosspred_temp$allRRfit[which.min(f_crosspred_temp$allRRfit)] %>% names %>% as.numeric
+    f_crosspred_temp <- 
+      crosspred(f_cb_temp,f_model_temp,
+                by=0.1,cen = f_MMT)
+    
+    # -------------------------------------------------------------------------
+    list(crossreduce_temp = f_crossreduce_temp,
+         crosspred_temp = f_crosspred_temp) %>% 
+      return()
+    
+  })) %>% 
+  
+  # coef & vcov of reduced parameters
+  mutate(first_coef_temp = map(first_prepre_temp,~{.x$crossreduce_temp %>% coef}),
+         first_vcov_temp = map(first_prepre_temp,~{.x$crossreduce_temp %>% vcov}),
+         crosspred_temp = map(first_prepre_temp,~{.x$crosspred_temp})) %>% 
+  select(-first_prepre_temp) %>% 
+  
+  
+  ### WBGT ###
+  mutate(first_prepre_WBGT = map(data, ~{
+    
+    
+    # data ------------------------------------------------------------------
+    f_data = .x
+    
+    # crossbasis ------------------------------------------------------------
+    f_cb_WBGT <- 
+      crossbasis(f_data$maxWBGT, 
+                 lag=laglag,
+                 argvar=list(fun=varfun,
+                             knots=quantile(f_data$maxWBGT,varper/100,na.rm=T),
+                             Bound = range(f_data$maxWBGT, na.rm = T)),
+                 arglag=list(fun=varfun,
+                             knots=logknots(laglag, lagnk)),
+                 group = f_data$year)
+    
+    # statmodel -------------------------------------------------------------
+    f_model_WBGT <- 
+      glm(all ~ f_cb_WBGT + year + dow + 
+            ns(dos,knots=quantile(dos_vec,seq(df_ns_dos)/(df_ns_dos+1))):factor(year),
+          data = f_data, 
+          family = quasipoisson)
+    
+    # crossreduce for first_stage ---------------------------------------------
+    # reference := mean() ~ (?ߋ??̐l?דI?????R???̎??Ґ?????
+    f_crossreduce_WBGT <- 
+      crossreduce(f_cb_WBGT, f_model_WBGT, 
+                  cen = mean(f_data$maxWBGT,na.rm=T),
+                  by = 0.1)
+    
+    # first-stage curve -------------------------------------------------------
+    f_crosspred_WBGT <- 
+      crosspred(f_cb_WBGT,f_model_WBGT,
+                by=0.1,cen = mean(f_data$maxWBGT,na.rm=T))
+    f_MMW <- 
+      f_crosspred_WBGT$allRRfit[which.min(f_crosspred_WBGT$allRRfit)] %>% names %>% as.numeric
+    f_crosspred_WBGT <- 
+      crosspred(f_cb_WBGT,f_model_WBGT,
+                by=0.1,cen = f_MMW)
+    
+    # -------------------------------------------------------------------------
+    list(crossreduce_WBGT = f_crossreduce_WBGT,
+         crosspred_WBGT = f_crosspred_WBGT) %>% 
+      return()
+    
+  })) %>% 
+  
+  # coef & vcov of reduced parameters
+  mutate(first_coef_WBGT = map(first_prepre_WBGT,~{.x$crossreduce_WBGT %>% coef}),
+         first_vcov_WBGT = map(first_prepre_WBGT,~{.x$crossreduce_WBGT %>% vcov}),
+         crosspred_WBGT = map(first_prepre_WBGT,~{.x$crosspred_WBGT})) %>% 
+  select(-first_prepre_WBGT)
+
+
+
+first_result_list
+
+
+# # visualization for first_pref47 -------------------------------------------
+# 
+# pdf("figure/Figure_first_pref47.pdf",width=8,height=4)
+# par(mfrow=c(1,2))
+# 
+# for(p in 1:47){
+#   first_result_list$crosspred_temp[[p]] %>%
+#     plot("overall",
+#          main=paste0(first_result_list$prefname[p],"\n(Temperature)"),
+#          ylim=c(0.95,1.5),
+#          xlab="Daily maximum temperature",
+#          ylab="Cumulative Relative Risk")
+#   first_result_list$crosspred_WBGT[[p]] %>%
+#     plot("overall",
+#          main=paste0(first_result_list$prefname[p],"\n(WBGT)"),
+#          ylim=c(0.95,1.5),
+#          xlab="Daily maximum WBGT",
+#          ylab="Cumulative Relative Risk")
+# }
+# 
+# dev.off()
+
+
+
+###############################################################################
+# 
+# BLUP from nationwide meta-analysis 
+#
+############################################################################### 
+
+metapredictor_table %>% colnames
+
+# -------------------------------------------------------------------------
+# meta-regression ---------------------------------------------------------
+# -------------------------------------------------------------------------
+nationwide_2stage_BLUP <- 
+  first_result_list %>% 
+  select(prefcode,prefname,data,region) %>% 
+  
+  mutate(high_pers = map(data,
+                         ~{data.frame(
+                           high_pers = paste0("per",seq(90,97.5,2.5)),
+                           tmax_pers = .x$tmax %>% quantile(probs=seq(90,97.5,2.5)/100) %>% round(1),
+                           maxWBGT_pers = .x$maxWBGT %>% quantile(probs=seq(90,97.5,2.5)/100) %>% round(1)
+                         )})) %>% 
+  
+  ### temp
+  mutate(nationwide_temp_BLUP = 
+           mixmeta(as.matrix(bind_rows(first_result_list$first_coef_temp)) ~ 
+                     mean_temperature + 
+                     sd_temperature,
+                   first_result_list$first_vcov_temp,
+                   random = ~1|region/prefname,
+                   data = metapredictor_table,
+                   control= list(showiter = T, igls.inititer = 10),
+                   method = "reml") %>% 
+           blup(vcov=T)) %>% 
+  
+  ### WBGT
+  mutate(nationwide_WBGT_BLUP = 
+           mixmeta(as.matrix(bind_rows(first_result_list$first_coef_WBGT)) ~ 
+                     mean_WBGT +
+                     sd_WBGT,
+                   first_result_list$first_vcov_WBGT,
+                   random = ~1|region/prefname,
+                   data = metapredictor_table,
+                   control = list(showiter = T, igls.inititer = 10),
+                   method = "reml") %>% 
+           blup(vcov=T))
+
+
+
+# -------------------------------------------------------------------------
+# Wald test ---------------------------------------------------------------
+# -------------------------------------------------------------------------
+
+temp_mixmeta <-
+  mixmeta(as.matrix(bind_rows(first_result_list$first_coef_temp)) ~
+            mean_temperature + 
+            sd_temperature,
+          first_result_list$first_vcov_temp,
+          random = ~1|region/prefname,
+          data = metapredictor_table,
+          control= list(showiter = T, igls.inititer = 10),
+          method = "reml")
+WBGT_mixmeta <-
+  mixmeta(as.matrix(bind_rows(first_result_list$first_coef_WBGT)) ~
+            mean_WBGT +
+            sd_WBGT,
+          first_result_list$first_vcov_WBGT,
+          random = ~1|region/prefname,
+          data = metapredictor_table,
+          control= list(showiter = T, igls.inititer = 10),
+          method = "reml")
+
+fwald <- function(model,var) {
+  ind <- grep(var,names(coef(model)))
+  coef <- coef(model)[ind]
+  vcov <- vcov(model)[ind,ind]
+  waldstat <- coef%*%solve(vcov)%*%coef
+  df <- length(coef)
+  return(1-pchisq(waldstat,df))
+}
+
+wald_result <- 
+  data.frame(
+    heat = c("Temperature","Temperature","WBGT","WBGT"),
+    meta = c("mean","range","mean","range"),
+    wald = c(fwald(temp_mixmeta,"mean_temperature")  %>% round(4),
+             fwald(temp_mixmeta,"sd_temperature") %>% round(4),
+             fwald(WBGT_mixmeta,"mean_WBGT")  %>% round(4),
+             fwald(WBGT_mixmeta,"sd_WBGT") %>% round(4))
+  )
+
+wald_result
+# save(wald_result,
+#      file="R_code/usingWBGT_data/wald_result.R")
+
+
+# -------------------------------------------------------------------------
+# bvar & MMX & MMXP -------------------------------------------------------
+# -------------------------------------------------------------------------
+
+nationwide_2stage_BLUP <- 
+  
+  nationwide_2stage_BLUP %>% 
+  
+  ### bvar_temp ###
+  mutate(bvar_temp = map(data,~{
+    
+    # percentile
+    predvar_temp <- quantile(.x$tmax,0:1000/1000,na.rm=T) %>% round(1)
+    argvar_temp <- list(x = predvar_temp, 
+                        fun = varfun,
+                        knots = quantile(.x$tmax, varper/100, na.rm = T),
+                        Bound = range(.x$tmax, na.rm = T))
+    bvar_temp <- do.call(onebasis, argvar_temp)
+    return(bvar_temp)
+    
+  })) %>% 
+  
+  ### MMT & MMTP ###
+  mutate(MMTP = map2_dbl(bvar_temp,nationwide_temp_BLUP,
+                         ~{seq(1,99,0.1)[which.min((.x %*% .y$blup)[which(seq(0,100,0.1) == 1):which(seq(0,100,0.1) == 99)])]})) %>% 
+  mutate(MMT = map2_dbl(data,MMTP,
+                        ~{quantile(.x$tmax, .y/100, na.rm = T)})) %>% 
+  
+  ### bvar_WBGT ###
+  mutate(bvar_WBGT = map(data,~{
+    
+    # percentile
+    predvar_WBGT <- quantile(.x$maxWBGT,0:1000/1000,na.rm=T) %>% round(1)
+    argvar_WBGT <- list(x = predvar_WBGT, 
+                        fun = varfun,
+                        knots = quantile(.x$maxWBGT, varper/100, na.rm = T),
+                        Bound = range(.x$maxWBGT, na.rm = T))
+    bvar_WBGT <- do.call(onebasis, argvar_WBGT)
+    return(bvar_WBGT)
+    
+  })) %>% 
+  
+  ### MMW & MMWP ###
+  mutate(MMWP = map2_dbl(bvar_WBGT,nationwide_WBGT_BLUP,
+                         ~{seq(1,99,0.1)[which.min((.x %*% .y$blup)[which(seq(0,100,0.1) == 1):which(seq(0,100,0.1) == 99)])]})) %>% 
+  mutate(MMW = map2_dbl(data,MMWP,
+                        ~{quantile(.x$maxWBGT, .y/100, na.rm = T)}))
+
+nationwide_2stage_BLUP %>% tail
+
+# -------------------------------------------------------------------------
+# crosspred
+# -------------------------------------------------------------------------
+
+nationwide_2stage_BLUP <- 
+  nationwide_2stage_BLUP %>% 
+  ### temp ###
+  mutate(crosspred_temp_BLUP = 
+           nationwide_2stage_BLUP %>% 
+           select(bvar_temp,nationwide_temp_BLUP,MMT) %>% 
+           pmap(function(bvar_temp,nationwide_temp_BLUP,MMT){
+             
+             crosspred(bvar_temp,
+                       coef=nationwide_temp_BLUP$blup,
+                       vcov=nationwide_temp_BLUP$vcov,
+                       model.link="log",
+                       by=0.1,
+                       cen=MMT) %>% 
+               return()
+             
+           })) %>% 
+ 
+  ### WBGT ###
+  mutate(crosspred_WBGT_BLUP = 
+           nationwide_2stage_BLUP %>% 
+           select(bvar_WBGT,nationwide_WBGT_BLUP,MMW) %>% 
+           pmap(function(bvar_WBGT,nationwide_WBGT_BLUP,MMW){
+             
+             crosspred(bvar_WBGT,
+                       coef=nationwide_WBGT_BLUP$blup,
+                       vcov=nationwide_WBGT_BLUP$vcov,
+                       model.link="log",
+                       by=0.1,
+                       cen=MMW) %>% 
+               return()
+             
+           })) %>% 
+  
+  mutate(region = region %>% 
+           as.factor %>% fct_relevel(region_vec_level),
+         prefname = prefname %>% 
+           as.factor %>% fct_relevel(pref_vec))
+
+nationwide_2stage_BLUP
+
+
+# -------------------------------------------------------------------------
+# visualization
+# -------------------------------------------------------------------------
+
+boldbold <- 1.2
+
+BLUP_temp_curve <-
+  function(p){
+    p_BLUP_temp <-
+      nationwide_2stage_BLUP %>%
+      filter(prefcode == p) %>%
+      select(prefname,MMT,MMTP,crosspred_temp_BLUP) %>%
+      mutate(across(.cols=contains("MM"),
+                    .fns=~{round(.x,1)}))
+
+    p_BLUP_temp$crosspred_temp_BLUP[[1]] %>%
+      plot(ylim=c(0.95,1.5),
+           cex.axis = boldbold,
+           cex.lab = boldbold,
+           cex.main = boldbold,
+           xlab="Daily maximum temperature (°C)",
+           ylab="Relative Risk",
+           family="sans")
+    title(glue::glue("({p}) {p_BLUP_temp$prefname} (Temperature)"),
+          font.main = 1,
+          cex.main = boldbold,
+          adj = 0,
+          family="sans")
+    # MMT
+    segments(x0=p_BLUP_temp$MMT, y0=0.1, x1=p_BLUP_temp$MMT, y1=1.48,
+             lty="dashed")
+    text(x=p_BLUP_temp$MMT,y=1.49,
+         family="sans",
+         cex = boldbold - 0.2,
+         labels=paste0("MMT = ",p_BLUP_temp$MMT," (",p_BLUP_temp$MMTP,"%)"))
+    
+  }
+
+#
+BLUP_WBGT_curve <-
+  function(p){
+    p_BLUP_WBGT <-
+      nationwide_2stage_BLUP %>%
+      filter(prefcode == p) %>%
+      select(prefname,MMW,MMWP,crosspred_WBGT_BLUP) %>%
+      mutate(across(.cols=contains("MM"),
+                    .fns=~{round(.x,1)}))
+
+    p_BLUP_WBGT$crosspred_WBGT_BLUP[[1]] %>%
+      plot(ylim=c(0.95,1.5),
+           cex.axis = boldbold,
+           cex.lab = boldbold,
+           cex.main = boldbold,
+           xlab="Daily maximum WBGT (°C)",
+           ylab="Relative Risk",
+           family="sans")
+    title(glue::glue("({p}) {p_BLUP_WBGT$prefname} (WBGT)"),
+          font.main = 1,
+          cex.main = boldbold,
+          adj = 0,
+          family="sans")
+    
+    # MMW
+    segments(x0=p_BLUP_WBGT$MMW, y0=0.1, x1=p_BLUP_WBGT$MMW, y1=1.48,
+             lty="dashed")
+    text(x=p_BLUP_WBGT$MMW,y=1.49,
+         family="sans",
+         cex = boldbold - 0.2,
+         labels=paste0("MMW = ",p_BLUP_WBGT$MMW," (",p_BLUP_WBGT$MMWP,"%)"))
+    
+  }
+#
+
+p=34
+BLUP_temp_curve(p)
+BLUP_WBGT_curve(p)
+
+
+
+
+# -------------------------------------------------------------------------
+# visualization for BLUP_pref47
+# -------------------------------------------------------------------------
+
+# pdf("figure/FigS1_BLUP_heat_mortality.pdf",width=7,height=10)
+# 
+# par(mfrow=c(4,2),
+#     mar = c(5,5,4,1),
+#     family="sans")
+# for(p in 1:47){
+#   BLUP_temp_curve(p)
+#   BLUP_WBGT_curve(p)
+# }
+# dev.off()
+
+# -------------------------------------------------------------------------
+# Tokyo
+# -------------------------------------------------------------------------
+
+# pdf("figure/Fig2_BLUP_heat_mortality_in_4prefectures.pdf",
+#     width=7,height=10)
+# par(mfrow=c(4,2),
+#     oma = c(0, 0.5, 0, 0),
+#     family="sans")
+# 
+# p=1
+# BLUP_temp_curve(p)
+# BLUP_WBGT_curve(p)
+# p=13
+# BLUP_temp_curve(p)
+# BLUP_WBGT_curve(p)
+# p=27
+# BLUP_temp_curve(p)
+# BLUP_WBGT_curve(p)
+# p=40
+# BLUP_temp_curve(p)
+# BLUP_WBGT_curve(p)
+# 
+# dev.off()
+
---
+++ 6_Table_metareg_usingWBGT.R
@@ -0,0 +1,151 @@
+# Table_metareg_usingWBGT.pdf作成Rcode
+
+load("R_code/usingWBGT_data/AF_usingWBGT_90th.R")
+load("R_code/usingWBGT_data/AF_usingWBGT_95th.R")
+load("R_code/usingWBGT_data/AF_usingWBGT_45prefectures_90th.R")
+load("R_code/usingWBGT_data/AF_usingWBGT_45prefectures_95th.R")
+load("R_code/usingWBGT_data/AF_usingWBGT_first_90th.R")
+load("R_code/usingWBGT_data/AF_usingWBGT_first_95th.R")
+load("R_code/usingWBGT_data/RR_usingWBGT.R")
+load("R_code/usingWBGT_data/RR_usingWBGT_45prefectures.R")
+load("R_code/usingWBGT_data/RR_usingWBGT_first.R")
+
+pacman::p_load(
+  tidyverse,
+  gt
+)
+ls()
+AF_usingWBGT_90th
+AF_usingWBGT_95th
+AF_usingWBGT_45prefectures_90th
+AF_usingWBGT_45prefectures_95th
+AF_usingWBGT_first_90th
+AF_usingWBGT_first_95th
+RR_usingWBGT
+RR_usingWBGT_45prefectures
+RR_usingWBGT_first
+
+
+
+  
+
+# -------------------------------------------------------------------------
+# gt作成 --------------------------------------------------------------------
+# -------------------------------------------------------------------------
+gt_metareg_usingWBGT <- 
+  bind_rows(
+    # RR ----------------------------------------------------------------------
+    bind_rows(
+      RR_usingWBGT %>% select(group,vars,Estimate,L95,U95,I2,I2_pre),
+      RR_usingWBGT_45prefectures %>% select(group,vars,Estimate,L95,U95,I2,I2_pre),
+      RR_usingWBGT_first %>% select(group,vars,Estimate,L95,U95,I2,I2_pre)
+    ) %>% 
+      mutate(across(.cols=all_of(c("Estimate","L95","U95")),
+                    .fns=~{round(.x*100,2)})) %>% 
+      mutate(vars = gsub("per","",vars)) %>% 
+      mutate(vars = paste0(vars,"th")) %>% 
+      filter(vars %in% c("90th","95th")) %>% 
+      select(group,vars,Estimate,L95,U95,I2,I2_pre) %>% 
+      mutate(indi = "RR"),
+    
+    # AF ----------------------------------------------------------------------
+    bind_rows(
+      AF_usingWBGT_90th,
+      AF_usingWBGT_95th,
+      AF_usingWBGT_45prefectures_90th,
+      AF_usingWBGT_45prefectures_95th,
+      AF_usingWBGT_first_90th,
+      AF_usingWBGT_first_95th
+    ) %>% 
+      as_tibble() %>% 
+      mutate(across(.cols=where(is.numeric),
+                    .fns = ~{round(.x,2)})) %>% 
+      select(group,vars,Estimate,L95,U95,I2,I2_pre) %>% 
+      mutate(vars = gsub(" cutoff","",vars)) %>% 
+      mutate(indi = "AF")
+    
+  ) %>% 
+  # decimal number
+  mutate(across(.cols=c("Estimate","L95","U95","I2","I2_pre"),
+                .fns=~{formatC(.x, digits = 2, format="f")})) %>% 
+  
+  mutate(CI_WBGT = glue::glue("({L95}%, {U95}%)"),
+         beta = glue::glue("{Estimate}%")) %>%  
+  select(group,vars,beta,CI_WBGT,I2,I2_pre,indi) %>% 
+  pivot_wider(id_cols = c("group","vars"),
+              names_from = "indi",
+              values_from = c("beta","CI_WBGT","I2","I2_pre")) %>% 
+  gt(groupname_col = "group",
+     rowname_col = "vars") %>% 
+  tab_options(
+    table.font.names = "Arial"
+  ) %>% 
+  tab_header(
+    title = md("Table 2"),
+    subtitle = md("The association between prefecture-level Relative Risk or Attributable Fraction, and indicator variable of WBGT from third-stage meta-regression analysis")
+  ) %>% 
+  opt_align_table_header(align = "left") %>% 
+  cols_width(
+    "vars" ~ px(80),
+    c("beta_RR","beta_AF") ~ px(80),
+    c("CI_WBGT_RR","CI_WBGT_AF") ~ px(130),
+    c("I2_RR","I2_AF","I2_pre_RR","I2_pre_AF") ~ px(70)
+  ) %>% 
+  cols_align(
+    align = "center", 
+    columns = TRUE
+  ) %>% 
+  tab_spanner(
+    label = md("Relative Risk"),
+    columns = contains("RR")
+  ) %>% 
+  tab_spanner(
+    label = md("Attributable Fraction"),
+    columns = contains("AF")
+  ) %>% 
+  cols_label(
+    beta_RR = md("*&beta;*"),
+    beta_AF = md("*&beta;*"),
+    CI_WBGT_RR = md("(95% CI)"),
+    CI_WBGT_AF = md("(95% CI)"),
+    I2_RR = md("*I<sup>2*"),
+    I2_AF = md("*I<sup>2*"),
+    I2_pre_RR = md("*I<sup>2*_pre"),
+    I2_pre_AF = md("*I<sup>2*_pre")
+  ) %>%
+  tab_source_note(
+    source_note = md("*Note that these results show the percentage change between the results from WBGT and those from temperature. Null association is zero, and positive values indicate the results from WBGT are higher than those for temperature.*")
+  ) %>% 
+  tab_footnote(
+    footnote = md("the residual heterogeneity"),
+    locations = cells_column_labels(
+      columns = c("I2_RR","I2_AF")
+    )
+  ) %>% 
+  tab_footnote(
+    footnote = md("the estimated coefficient of indicator variable for WBGT"),
+    locations = cells_column_labels(
+      columns = c("beta_RR","beta_AF")
+    )
+  ) %>% 
+  tab_footnote(
+    footnote = md("sensitivity analysis for removing Hokkaido and Okinawa"),
+    locations = cells_row_groups(
+      groups = "45prefectures"
+    )
+  )%>% 
+  tab_footnote(
+    footnote = md("sensitivity analysis for using first-stage heat-mortality association"),
+    locations = cells_row_groups(
+      groups = "first"
+    )
+  ) %>% 
+  opt_footnote_marks(marks = "standard")
+
+gt_metareg_usingWBGT
+# save --------------------------------------------------------------------
+setwd("C:/Users/u030t/OneDrive/デスクトップ/research proposal/temp_WBGT")
+gtsave(gt_metareg_usingWBGT,
+       "figure/Table2_metareg_usingWBGT.pdf",
+       zoom=0.7)
+
